58eab2ae795ea12edd766848e75f00d61c68324b
bishnu bhusal <bishnu_bhusal83@yahoo.com>
2023-01-21 12:47:33 -0700

    multiplied two numbers from user.

--- hi.py
+++ hi.py
@@ -10,3 +10,8 @@ b = 6
 c = a + b
 
 print("The value is: ", c)
+num = (int(input("Enter a number: ")))
+print(num)
+num2 = (int(input("Enter second number: ")))
+int result = num * num2
+print(result)
